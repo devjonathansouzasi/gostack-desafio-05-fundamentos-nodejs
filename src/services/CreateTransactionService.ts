4548fc1bba5983c660a0615bea8b0a095278f08d
Jonathan Souza <devjonathansouzasi@gmail.com>
2020-08-24 18:14:24 -0300

    Desafio concluído

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,5 +1,12 @@
+import { uuid } from 'uuidv4';
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
+
+interface Request {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+}
 
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
@@ -8,8 +15,14 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, type, value }: Request): Transaction {
+    const { total } = this.transactionsRepository.getBalance();
+    if (type === 'outcome' && value > total) {
+      throw new Error('You do not have enough balance');
+    }
+    const transaction = { title, type, value };
+    const createdTransaction = this.transactionsRepository.create(transaction);
+    return createdTransaction;
   }
 }
 
